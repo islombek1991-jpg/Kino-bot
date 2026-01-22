f00f1e9dc0aebff36f66b4273ab288d06a622c99
islombek1991-jpg <islombekerkaboyev1@gmail.com>
2026-01-22 21:11:52 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,21 +1,18 @@
-import os
-import logging
-from aiogram import Bot, Dispatcher, types
-from aiogram.utils import executor
 
-logging.basicConfig(level=logging.INFO)
+import os
+from aiogram import Bot, Dispatcher, executor, types
 
 TOKEN = os.getenv("BOT_TOKEN")
 
 if not TOKEN:
-    raise RuntimeError("BOT_TOKEN topilmadi! Railway Variables ga BOT_TOKEN ni kiriting.")
+    raise ValueError("BOT_TOKEN topilmadi")
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
 
 @dp.message_handler(commands=["start"])
-async def start(m: types.Message):
-    await m.answer("✅ Bot ishlayapti. Railway + GitHub tayyor.")
+async def start_handler(message: types.Message):
+    await message.answer("✅ Bot ishlayapti. Railway tayyor.")
 
 if __name__ == "__main__":
     executor.start_polling(dp, skip_updates=True)
